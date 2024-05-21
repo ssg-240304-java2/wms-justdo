c697ffca3dc88eac435fd86b080120cf91932352
speter6501 <speter650@gmail.com>
2024-05-21 12:02:58 +0900

    수정 DTO toString

--- src/main/java/com/justdo/fruitfruit/model/dto/ProductDTO.java
+++ src/main/java/com/justdo/fruitfruit/model/dto/ProductDTO.java
@@ -65,5 +65,4 @@ public class ProductDTO {
                 ", 무게 = " + productWeight +
                 ']';
     }
-
 }
--- src/main/java/com/justdo/fruitfruit/model/dto/UserDTO.java
+++ src/main/java/com/justdo/fruitfruit/model/dto/UserDTO.java
@@ -2,11 +2,12 @@ package com.justdo.fruitfruit.model.dto;
 
 import lombok.*;
 
+import java.text.DecimalFormat;
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
-@ToString
 public class UserDTO {
     private int userSeq;
     private String id;
@@ -23,5 +24,31 @@ public class UserDTO {
         this.address = address;
         this.phoneNumber = phoneNumber;
         this.auth = auth;
+    }
+
+    @Override
+    public String toString() {
+        String authName;
+        if (auth == 1) {
+            authName = "시스템관리자";
+        } else if (auth == 2) {
+            authName = "창고관리자";
+        } else if (auth == 3) {
+            authName = "구매자";
+        } else if (auth ==4) {
+            authName = "판매자";
+        } else {
+            authName = "unKnown";
+        }
+
+        return
+                "[회원번호] " + userSeq + " | " +
+                "[아이디] " + id + " " +
+                "[비밀번호] " + password + " " +
+                "[이름] " + name + " " +
+                "[주소] " + address + " " +
+                "[전화번호] " + phoneNumber + " " +
+                "[권한] " + authName
+                ;
     }
 }
